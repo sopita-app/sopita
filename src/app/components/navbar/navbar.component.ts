b2b3c782d7f92c01dfe591e9f5e9db23e95201d2
romina3 <romina@acuidado.com>
2021-06-16 21:22:14 -0300

    nav bar choreada

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
-
+export class NavbarComponent {
+  showNavBar:boolean=false
   constructor() { }
 
-  ngOnInit(): void {
+  toggleNavBar(){
+    this.showNavBar = !this.showNavBar
   }
 
 }
--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { FormComponent } from './form/form.component';
 import { TaskComponent } from './task/task.component';
 import { TaskListComponent } from './task-list/task-list.component';
-import { ComponentsModule } from '../components/components.module';
 
 
 
@@ -15,7 +14,6 @@ import { ComponentsModule } from '../components/components.module';
   ],
   imports: [
     CommonModule,
-    ComponentsModule
   ],
   exports: [
     FormComponent,
