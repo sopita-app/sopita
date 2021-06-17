bc8abd879e969b6bea92e5e5302f32ec3259749f
Daniel-Ignacio-Nieto <ignacionieto65@hotmail.com>
2021-06-17 17:04:37 -0300

    merge del dev

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,22 +5,24 @@ import { SigninComponent } from './auth/signin/signin.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { TaskListComponent } from './tasks/task-list/task-list.component';
 
-const routes : Routes=[
-  {path: 'auth', 
-  loadChildren: ()=> import('./auth/auth.module').then( m => m.AuthModule)
-},
-{
-  path: '', 
-  loadChildren:() =>import('./tasks/tasks.module').then(m => m.TasksModule),
-  canLoad: [AuthGuard],
-  canActivate: [AuthGuard]
-},
-{path: '**', redirectTo: 'auth'}
-]
-
+const routes: Routes = [
+	{
+		path: 'auth',
+		loadChildren: () =>
+			import('./auth/auth.module').then((m) => m.AuthModule),
+	},
+	{
+		path: '',
+		loadChildren: () =>
+			import('./tasks/tasks.module').then((m) => m.TasksModule),
+		canLoad: [AuthGuard],
+		canActivate: [AuthGuard],
+	},
+	{ path: '**', redirectTo: 'auth' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
