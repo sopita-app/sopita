a203a246da04369494ee64c6b1e7b4804e6ad88c
romina3 <romina@acuidado.com>
2021-06-18 18:34:40 -0300

    persistencia de usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,10 @@ import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 export class AppComponent {
   title = 'agendaApp';
   faTrashAlt = faTrashAlt;
+
+  constructor(private authService: AuthService){
+    if(localStorage.getItem('user')){
+      this.authService.logFromLS(localStorage.getItem('user'))
+    }
+  }
 }
