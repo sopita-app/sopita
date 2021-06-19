e79d123199fa5294db1d39275b9b1b2196bbf72f
viki-almiron <vikialmiron@gmail.com>
2021-06-19 01:15:15 -0300

    modal fixes

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import {Routes, RouterModule} from '@angular/router'
+import { Routes, RouterModule } from '@angular/router'
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 
--- src/app/tasks/form/form.component.ts
+++ src/app/tasks/form/form.component.ts
@@ -3,6 +3,7 @@ import { NewTask } from 'src/app/interfaces/task.interface';
 import { AuthService } from 'src/app/auth/auth.service';
 import { TasksService } from '../tasks.service';
 import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
 	selector: 'app-form',
@@ -20,17 +21,18 @@ export class FormComponent  {
 	constructor(
 		private taskService: TasksService,
 		private authService: AuthService,
-		private router: Router
+		private router: Router,
+		private _snackBar: MatSnackBar
 	) {}
 
 	addTask() {
 		//usamos esta funcion para recibir los datos del formulario y accionar la creacion de la tarea al backend
 		// ejecutamos la creacion de la tarea con los datos en la variable
 		
-		// this.authService.loggedUser._id
 		this.taskService.postTask({task: this.formData, userId: this.authService.loggedUser._id}).subscribe((res: any) => {
 			console.log(res)
 			if(res.success){
+				this._snackBar.open('Tarea agregada con éxito', 'Ok', {duration: 1500});
 				this.router.navigate(['./home']);
 			}
 		}) 
